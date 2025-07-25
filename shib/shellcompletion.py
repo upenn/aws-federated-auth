4a218bc37257cbe2a0f8471b821b4f61b23877f4
Roger Chu <rchu@upenn.edu>
2025-07-25 15:23:15 -0400

    move the closing double quotation for omz completion into compadd call

--- shib/shellcompletion.py
+++ shib/shellcompletion.py
@@ -41,9 +41,9 @@ _aws_profile_export() {
     # filter for the current value
     # add end quote to each profile
     profiles=(${(f)"$(grep "^\[$curval" ~/.aws/credentials 2>/dev/null \
-                   | sed -e 's/^\[\(.*\)\]$/\\1\"/')"})
+                   | sed -e 's/^\[\(.*\)\]$/\\1/')"})
     
-    compadd -Q -P AWS_PROFILE=\" -U -- ${profiles[@]}
+    compadd -Q -P AWS_PROFILE=\" -S '"' -U -- ${profiles[@]}
     return
   fi
 
